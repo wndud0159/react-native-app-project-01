e27945d06f032d55697cdf8e98efa0aea5c09a29
최주영 <73331008+wndud0159@users.noreply.github.com>
2023-01-01 01:20:55 +0900

    Delete project02 directory

--- project02/TurryUpApp/declaration.d.ts
+++
@@ -1,8 +0,0 @@
-// declaration.d.ts
-
-declare module '*.svg' {
-  import React from 'react';
-  import {SvgProps} from 'react-native-svg';
-  const content: React.FC<SvgProps>;
-  export default content;
-}
